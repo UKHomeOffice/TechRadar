fedb3d9d05cf7c593b561615a1f55426d11bf024
Tim Gent <tim.gent@gmail.com>
2017-11-01 16:15:37 +0000

    Add switch so you can run without Azure AD - Required for one click docker-compose without Azure AD

--- utils/passport.js
+++ utils/passport.js
@@ -71,66 +71,68 @@ passport.use(new LocalStrategy(
     }));
 
 // Used by ADFS users
-passport.use(new OIDCStrategy(strategyConfig,
-    function (profile, done) {
-        // Depending on the type of the account e.g. registered in live.com or kainos.com
-        // user's email may be returned in "unique_name" field instead of "email"
-        var email = profile._json.email || profile._json.unique_name
-        if (!email) {
-            return done(new Error("No email found"), null);
-        }
+if (process.env.AZURE_IDENTITY_METADATA) {
+    passport.use(new OIDCStrategy(strategyConfig,
+        function (profile, done) {
+            // Depending on the type of the account e.g. registered in live.com or kainos.com
+            // user's email may be returned in "unique_name" field instead of "email"
+            var email = profile._json.email || profile._json.unique_name
+            if (!email) {
+                return done(new Error("No email found"), null);
+            }
 
-        process.nextTick(function () {
-            // ADFS user's username is set to the email used in the authentication process
-            users.findByUsername(email, function (err, user) {
-                if (err) {
-                    return done(err);
-                }
-                if (!user) {
-                    registerUserUsingProfileData(profile._json, function(err, user) {
-                        if(err){
-                            return done(err);
-                        }
-                        log.info("A new user has been created. Email: " + user.email);
+            process.nextTick(function () {
+                // ADFS user's username is set to the email used in the authentication process
+                users.findByUsername(email, function (err, user) {
+                    if (err) {
+                        return done(err);
+                    }
+                    if (!user) {
+                        registerUserUsingProfileData(profile._json, function(err, user) {
+                            if(err){
+                                return done(err);
+                            }
+                            log.info("A new user has been created. Email: " + user.email);
+                            return done(null, user);
+                        });
+                    }
+                    else if (!user.enabled) {
+                        return done(null, false);
+                    }
+                    else {
+                        log.info("An existing user is logging in using email: " + user.email);
+                        log.debug(user);
                         return done(null, user);
-                    });
-                } 
-                else if (!user.enabled) {
-                    return done(null, false);
-                }
-                else {
-                    log.info("An existing user is logging in using email: " + user.email);
-                    log.debug(user);
-                    return done(null, user);
-                }
+                    }
+                });
             });
+        }
+    ));
+
+    /**
+     * Reads profile data sent by Azure AD and persists the user in the DB
+     * @param  {Object}   profileJson Azure AD profile data such as email, unique_name, etc.
+     * @param  {Function} done        Callback function that returns the user from our DB or an error
+     */
+    function registerUserUsingProfileData(profileJson, done) {
+        log.info("Registering a new user with email: " + profileJson.email);
+        var email = profileJson.email || profileJson.unique_name;
+        // password is required, so we need to provide one even when the true password is handled by ADFS
+        var randomPassword = require('crypto').randomBytes(256).toString();
+        var newUser = new User(null, profileJson.unique_name, email, profileJson.name, randomPassword, null, 2, true);
+        users.add(newUser, function(userId, error){
+            log.info("Getting user with id = " + userId + " from the database");
+            users.findById(userId, function(err, user){
+                if(err){
+                    return done(err, null);
+                }
+                return done(null, user);
+            });
+            if(error){
+                log.error(error);
+            }
         });
     }
-));
-
-/**
- * Reads profile data sent by Azure AD and persists the user in the DB
- * @param  {Object}   profileJson Azure AD profile data such as email, unique_name, etc.
- * @param  {Function} done        Callback function that returns the user from our DB or an error
- */
-function registerUserUsingProfileData(profileJson, done) {
-    log.info("Registering a new user with email: " + profileJson.email);
-    var email = profileJson.email || profileJson.unique_name;
-    // password is required, so we need to provide one even when the true password is handled by ADFS
-    var randomPassword = require('crypto').randomBytes(256).toString();
-    var newUser = new User(null, profileJson.unique_name, email, profileJson.name, randomPassword, null, 2, true);
-    users.add(newUser, function(userId, error){
-        log.info("Getting user with id = " + userId + " from the database");
-        users.findById(userId, function(err, user){
-            if(err){
-                return done(err, null);
-            }
-            return done(null, user);
-        });
-        if(error){
-            log.error(error);
-        }
-    });
 }
 
 passport.serializeUser(function (user, cb) {
